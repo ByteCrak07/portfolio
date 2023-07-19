59c78c210485b821b20ad2333b61a13a7153c422
ByteCrak07 <abilsavio@gmail.com>
2023-07-19 13:40:40 +0530

    Edit experience

--- components/app/experience.tsx
+++ components/app/experience.tsx
@@ -23,7 +23,16 @@ const Experience: FC = () => {
           <h3 className='text-2xl font-semibold text-white'>
             Full Stack Developer Intern
           </h3>
-          <h4 className='text-lg text-slate-200'>@ BuildBear Labs</h4>
+          <h4 className='text-lg text-slate-200'>
+            <a
+              href='https://www.buildbear.io'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='hover:text-white'
+            >
+              @ BuildBear Labs
+            </a>
+          </h4>
           <p className='my-4 text-justify text-base text-slate-400'>
             BuildBear specializes in creating highly customizable private
             testnets specifically designed for decentralized applications
@@ -36,7 +45,26 @@ const Experience: FC = () => {
           </p>
           <ul className='ml-6 text-slate-400'>
             <li className='my-2 list-disc'>
-              Worked primarily on their backend and BuildBear Utils.
+              Worked mostly on their backend and infrastructure.
+            </li>
+            <li className='my-2 list-disc'>
+              Spearheaded the development of{' '}
+              <a
+                href='https://utils.buildbear.io'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='underline hover:text-white'
+              >
+                Buildbear Utils
+              </a>{' '}
+              project which contains a lot of cool stuff like impersonating any
+              wallet address, interacting with contracts and dapps with
+              impersonation, taking snapshots, and fast-forwarding time on
+              Buildbear testnets
+            </li>
+            <li className='my-2 list-disc'>
+              Created a Slack app for getting stats and server health-check
+              notifications.
             </li>
             <li className='my-2 list-disc'>
               Worked with Express, TypeScript, Docker, Kubernetes & Next.JS.
@@ -80,12 +108,71 @@ const Experience: FC = () => {
         <li className='mb-10 ml-4'>
           <div className='bg-glow absolute -left-[7px] mt-1.5 h-3 w-3 rounded-full border border-slate-800'></div>
           <time className='text-glow mb-1 text-sm font-normal leading-none'>
+            Jan 2022
+          </time>
+          <h3 className='text-2xl font-semibold text-white'>
+            Chief Technology Officer
+          </h3>
+          <h4 className='text-lg text-slate-200'>
+            <a
+              href='https://cryptoliterature.in'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='hover:text-white'
+            >
+              @ Cryptoliterature
+            </a>
+          </h4>
+          <p className='my-4 text-justify text-base text-slate-400'>
+            Cryptoliterature is an NFT marketplace for writers where writers can
+            publish their works, write blogs and sell them as NFTs. Currently
+            its in beta. Support for both ethereum and polygon NFTs during beta.
+          </p>
+          <ul className='ml-6 text-slate-400'>
+            <li className='my-2 list-disc'>
+              Built using Next.JS and NestJS along with Prisma for ORM. Deployed
+              using Vercel and AWS.
+            </li>
+            <li className='my-2 list-disc'>
+              As of now writers can onboard themselves and create blogs, which
+              will be displayed on their profile.
+            </li>
+            <li className='my-2 list-disc'>
+              Onboarded around 100 writers on the platform during the first beta
+              release.
+            </li>
+            <li className='my-2 list-disc'>
+              Website:{' '}
+              <a
+                href='https://cryptoliterature.in'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='underline hover:text-white'
+              >
+                cryptoliterature.in
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li className='mb-10 ml-4'>
+          <div className='bg-glow absolute -left-[7px] mt-1.5 h-3 w-3 rounded-full border border-slate-800'></div>
+          <time className='text-glow mb-1 text-sm font-normal leading-none'>
             June 2021
           </time>
           <h3 className='text-2xl font-semibold text-white'>
             Software Developer
           </h3>
-          <h4 className='text-lg text-slate-200'>@ TATOS Technologies</h4>
+          <h4 className='text-lg text-slate-200'>
+            <a
+              href='https://tatos.tech'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='hover:text-white'
+            >
+              @ TATOS Technologies
+            </a>
+          </h4>
           <p className='my-4 text-justify text-base text-slate-400'>
             TATOS is a group of highly skilled software developers who possess a
             visionary mindset aimed at bringing positive change to the world.
@@ -142,7 +229,16 @@ const Experience: FC = () => {
             February 2021
           </time>
           <h3 className='text-2xl font-semibold text-white'>IEEE Webmaster</h3>
-          <h4 className='text-lg text-slate-200'>@ IEEE GEC Thrissur</h4>
+          <h4 className='text-lg text-slate-200'>
+            <a
+              href='https://ieee.gectcr.ac.in'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='hover:text-white'
+            >
+              @ IEEE GEC Thrissur
+            </a>
+          </h4>
           <p className='my-4 text-justify text-base text-slate-400'>
             Served as webmaster for IEEE GEC Thrissur for the year 2021. I was
             among a team of 3 webmasters. Today the website is maintained by my
